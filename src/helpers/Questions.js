a274c7f528c75d0b1757ea3c6fbcbbbf57423ec9
Anand Medar <anandgowda704@gmail.com>
2021-01-30 14:04:38 +0530

    quiz

---
+++ src/helpers/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+    {
+      prompt: "What is my name?",
+      optionA: "Aravind",
+      optionB: "Arjun",
+      optionC: "Anand",
+      optionD: "Tarak",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Who is my favourite actor?",
+      optionA: "Allu Arjun",
+      optionB: "Jr NTR",
+      optionC: "Prabhas",
+      optionD: "Vishwak Sen",
+      asnwer: "optionB",
+    },
+    {
+      prompt: "Which is my favourite technology?",
+      optionA: "Reactjs",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionA",
+    },
+  ];
